4dd8749c6527fff2efd610c0c567cd8d60b6e350
bantoneli <bantoneli@outlook.com>
2024-05-04 18:55:16 -0300

    change profile name and setting img alt using JS

--- script.js
+++ script.js
@@ -14,8 +14,16 @@ function toggleMode() {
   if (html.classList.contains("lightMode")) {
     // se tiver light mode, adicionar avatar-light.png
     img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto do Mayk Brito sorrindo, de óculos e barba com fundo amarelo."
+    )
   } else {
     // se não, adicionar avatar.png
     img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto do Mayk Brito de óculos escuros com fundo roxo e azul."
+    )
   }
 }
